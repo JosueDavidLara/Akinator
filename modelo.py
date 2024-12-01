1bd2aa466cd0d604cc9b17ee045f78feff89e3f9
Diana Vega <diana.vega@unah.hn>
2024-11-30 18:25:15 -0600

    refactor: adding animals, max_depth incremented.

--- modelo.py
+++ modelo.py
@@ -1,27 +1,33 @@
+# ----------------------------------IMPORTACION DE LIBRERIAS----------------------------------
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.tree import DecisionTreeClassifier, plot_tree, _tree
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+
+# ----------------------------------CREACION DEL MODELO----------------------------------
 
 # Cargar los datos
 data = pd.read_csv("./dataset_animales.csv")
 
-# Seleccionar las columnas con variables categóricas en variables dummy
+# Seleccionar las columnas con variables categóricas para convertirlas en variables dummy
 categorical_columns = [
     "Clasificacion",
     "Locomocion",
     "Reproduccion",
     "Tamano",
     "Dieta",
+    "Patas",
     "Piel",
     "Caracteristica",
 ]
 
 # Usar pd.get_dummies para convertir las columnas categóricas en variables dummy
-data = pd.get_dummies(data, columns=categorical_columns, drop_first=True)
+data = pd.get_dummies(
+    data, columns=categorical_columns, drop_first=True, prefix="", prefix_sep=""
+)
 
 # Mostrar el dataframe procesado
-print(data.info())
+# print(data.info())
 
 # Separar las características (X) y la etiqueta (y)
 X = data.drop(columns=["Animal"])
@@ -31,16 +37,17 @@ y = data["Animal"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.7)
 
 # Crear y entrenar el modelo de árbol de decisión
-model = DecisionTreeClassifier(max_depth=27)
+model = DecisionTreeClassifier(max_depth=34)
 model.fit(X, y)
 
+# ----------------------------------EVALUACION DEL MODELO----------------------------------
 
 # Hacer predicciones (testear el modelo)
 pred = model.predict(X_test)
 comparativa = {"predicciones": pred, "valor real": y_test}
 result = pd.DataFrame(comparativa)
 print(result)
-print("Precisión del modelo:", model.score(X_test, y_test))
+print(f"Precisión del modelo: {(model.score(X_test, y_test)*100):.2f}%")
 
 # Visualizar el árbol de decisión (opcional)
 # plt.figure(figsize=(20, 10))
